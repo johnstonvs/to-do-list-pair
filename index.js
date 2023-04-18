58b3248afc8f0bd5dc93e8d37d6c63caf3de1147
johnston <johnstonvs@gmail.com>
2023-04-18 14:24:45 -0500

    Fixes adding list items functionality

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
+// Assigns various elements to variables
+let olElem = document.querySelector('ol');
+let addButton = document.querySelector("#add");
+let item = document.querySelector("#addItem");
+let form = document.querySelector('form');
+
 const addItem = string => {
   //creates html element by tag name
+  // Creates li element
   let liElem = document.createElement('li');
-  let inner = document.createTextNode(`${string}`);
-  liElem.appendChild(inner);
+  let liInner = document.createTextNode(`${string}`);
+  liElem.appendChild(liInner);
+
+  // Creates button element and adds to li element
+  let buttonElem = document.createElement('button');
+  let buttonInner = document.createTextNode('Remove');
+  buttonElem.appendChild(buttonInner);
+  liElem.appendChild(buttonElem);
+
+  // Appends li element to ol
   olElem.appendChild(liElem);
 }
 
-// Assigns various elements to variables
-let olElem = document.querySelector('ol');
+// add functionality to remove button
 
-let addButton = document.querySelector("#add");
-
-let item = document.querySelector("#addItem");
-
-let form = document.querySelector('form');
-
-//
-let input ='add item';
-
+let input;
 item.addEventListener("input", (eventObject) => {input = eventObject.target.value});
 
-if (typeof input === 'string') {
-  form.addEventListener("submit", addItem(input));
-} else {
-  form.addEventListener("submit", alert('Enter a string'));
-}
+addButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  addItem(input);
+});
